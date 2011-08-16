3b54b986645fa3fc9b92e954b48b3c3e06a7d64c
jhoblitt <jhoblitt@cpan.org>
2011-08-16 05:34:41 +0000

    Instead of disabling comments by default, add a new default page + no comments and a full-width + no comments template.

--- full-width-no-comments-page.php
+++ full-width-no-comments-page.php
@@ -1,23 +1,23 @@
 <?php
 /**
+ * Template Name: Full-width, no comments
+ * Description: A full-width template with no sidebar or comments
+ *
  * @package WordPress
- * @subpackage YokoGek
+ * @subpackage YokoGeek
  */
 
 get_header(); ?>
 
 <div id="wrap">
-<div id="main">
 
-	<div id="content">
+			<div id="content" class="full-width">
 
 				<?php the_post(); ?>
-
 				<?php get_template_part( 'content', 'page' ); ?>
-
+				
 				<?php #comments_template( '', true ); ?>
-
-	</div><!-- end content -->
-
-<?php get_sidebar(); ?>
+				
+			</div><!-- end content -->
+				
 <?php get_footer(); ?>
