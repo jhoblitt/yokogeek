b9916af2a3df59b9987e246f86f5dffba0abadce
jhoblitt <jhoblitt@cpan.org>
2011-08-16 07:17:45 +0000

    add the child theme name to the footer

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Yoko
+ */
+?>
+
+</div><!-- end wrap -->
+
+	<footer id="colophon" class="clearfix">
+		<p>Proudly powered by <a href="http://wordpress.org/" rel="generator">WordPress</a><span class="sep"> | </span><?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/wordpress-themes/" rel="designer">Elmastudio</a>' ); ?></p>
+		<a href="#page" class="top"><?php _e( 'Top', 'yoko' ); ?></a>
+	</footer><!-- end colophon -->
+	
+</div><!-- end page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
