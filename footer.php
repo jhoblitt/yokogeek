5d8e10c2af508c3923a59f92b562278f08d3ac4b
jhoblitt <jhoblitt@cpan.org>
2011-08-22 00:54:57 +0000

    re-fix footer after loosing changes with git rebase :)

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package WordPress
- * @subpackage Yoko
+ * @subpackage YokoGeek
  */
 ?>
 
 </div><!-- end wrap -->
 
 	<footer id="colophon" class="clearfix">
-		<p>Proudly powered by <a href="http://wordpress.org/" rel="generator">WordPress</a><span class="sep"> | </span><?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/wordpress-themes/" rel="designer">Elmastudio</a>' ); ?></p>
+		<p>Proudly powered by <a href="http://wordpress.org/" rel="generator">WordPress</a><span class="sep"> | </span><?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/wordpress-themes/" rel="designer">Elmastudio</a>' ); ?><span class="sep"> | </span> <?php printf( __( 'Theme: %1$s by %2$s', 'yokogeek' ), 'YokoGeek', '<a href="http://joshua.hoblitt.com/" rel="designer">Joshua Hoblitt</a>' ); ?></p>
 		<a href="#page" class="top"><?php _e( 'Top', 'yoko' ); ?></a>
 	</footer><!-- end colophon -->
 	
