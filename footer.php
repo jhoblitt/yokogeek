a5adbf2b404de2352f8df1cbde5f5f497db4f7f9
jhoblitt <jhoblitt@cpan.org>
2011-09-04 19:06:20 +0000

    link the footer to the github repo for YokoGeek

--- footer.php
+++ footer.php
@@ -8,12 +8,15 @@
 </div><!-- end wrap -->
 
 	<footer id="colophon" class="clearfix">
-		<p>Proudly powered by <a href="http://wordpress.org/" rel="generator">WordPress</a><span class="sep"> | </span><?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/wordpress-themes/" rel="designer">Elmastudio</a>' ); ?><span class="sep"> | </span> <?php printf( __( 'Theme: %1$s by %2$s', 'yokogeek' ), 'YokoGeek', '<a href="http://joshua.hoblitt.com/" rel="designer">Joshua Hoblitt</a>' ); ?></p>
+		<p>Proudly powered by <a href="http://wordpress.org/" rel="generator">WordPress</a><span class="sep"> | </span><?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/wordpress-themes/" rel="designer">Elmastudio</a>' ); ?><span class="sep"> | </span> <?php printf( __( 'Theme: %1$s by %2$s', 'yokogeek' ), '<a href="https://github.com/jhoblitt/yokogeek">YokoGeek</a>', '<a href="http://joshua.hoblitt.com/" rel="designer">Joshua Hoblitt</a>' ); ?></p>
 		<a href="#page" class="top"><?php _e( 'Top', 'yoko' ); ?></a>
 	</footer><!-- end colophon -->
 	
 </div><!-- end page -->
 <?php wp_footer(); ?>
+
+<script language=javascript src="/js/awstats_misc_tracker.js"></script>
+<noscript><img src="/js/awstats_misc_tracker.js?nojs=y" height=0 width=0 border=0 style="display: none"></noscript>
 
 </body>
 </html>
