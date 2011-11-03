232bf2d5b009cbdb2cde9b454bbb22af739bcdf1
jhoblitt <jhoblitt@cpan.org>
2011-11-03 16:03:26 +0000

    refer to this theme as a "child theme" in the footer

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 </div><!-- end wrap -->
 
 	<footer id="colophon" class="clearfix">
-		<p>Proudly powered by <a href="http://wordpress.org/" rel="generator">WordPress</a><span class="sep"> | </span><?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/wordpress-themes/" rel="designer">Elmastudio</a>' ); ?><span class="sep"> | </span> <?php printf( __( 'Theme: %1$s by %2$s', 'yokogeek' ), '<a href="https://github.com/jhoblitt/yokogeek">YokoGeek</a>', '<a href="http://joshua.hoblitt.com/" rel="designer">Joshua Hoblitt</a>' ); ?></p>
+		<p>Proudly powered by <a href="http://wordpress.org/" rel="generator">WordPress</a><span class="sep"> | </span><?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/wordpress-themes/" rel="designer">Elmastudio</a>' ); ?><span class="sep"> | </span> <?php printf( __( 'Child Theme: %1$s by %2$s', 'yokogeek' ), '<a href="https://github.com/jhoblitt/yokogeek">YokoGeek</a>', '<a href="http://joshua.hoblitt.com/" rel="designer">Joshua Hoblitt</a>' ); ?></p>
 		<a href="#page" class="top"><?php _e( 'Top', 'yoko' ); ?></a>
 	</footer><!-- end colophon -->
 	
